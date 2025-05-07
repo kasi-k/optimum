1a802e47d670761caef94f3cbb898aaf07747ec1
kasi-k <kasivishwanath698@gmail.com>
2025-05-07 15:50:41 +0530

    leads

--- src/pages/leads/campaign/LeadsTable.jsx
+++ src/pages/leads/campaign/LeadsTable.jsx
@@ -3,7 +3,7 @@ import { LuEye } from "react-icons/lu";
 import { HiArrowsUpDown } from "react-icons/hi2";
 import { FiTrash2 } from "react-icons/fi";
 import DeleteModal from "../../../component/DeleteModal";
-import { FinanceData, LeadsTableData } from "../../../component/Data";
+import {  LeadsTableData } from "../../../component/Data";
 
 
 const LeadsTable = () => {
@@ -11,7 +11,8 @@ const LeadsTable = () => {
 
   return (
     <div className="">
-      <div className="font-layout-font overflow-auto no-scrollbar h-48">
+      <div className="font-layout-font ">
+        <div className="sticky top-0 z-10">
         <table className="w-full  dark:text-white whitespace-nowrap">
           <thead >
             <tr className="font-semibold text-sm dark:bg-layout-dark bg-layout-light border-b-2 dark:border-overall_bg-dark border-overall_bg-light">
@@ -34,6 +35,10 @@ const LeadsTable = () => {
               <th className="pr-2 rounded-r-lg">Action</th>
             </tr>
           </thead>
+          </table>
+          </div>
+          <div className="overflow-auto no-scrollbar h-48 ">
+            <table className="w-full whitespace-nowrap ">
           <tbody className="dark:bg-layout-dark bg-layout-light rounded-2xl dark:text-gray-200 text-gray-600 cursor-default">
             {LeadsTableData && LeadsTableData.map((data,index) => (
               <tr
@@ -67,6 +72,7 @@ const LeadsTable = () => {
             ))}
           </tbody>
         </table>
+        </div>
       </div>
 
       {deleteModal && (
